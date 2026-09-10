63d779679c0862c863d9732a3efc1d86aaf1a354
Hari Shankar <harishankarhs351@gmail.com>
2026-09-10 11:58:15 +0530

    Added Frontend and Backend URL to the Application

--- skillsfusion-backend/src/main/java/com/example/skillsfusion_backend/config/WebConfig.java
+++ skillsfusion-backend/src/main/java/com/example/skillsfusion_backend/config/WebConfig.java
@@ -9,13 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     // Reads FRONTEND_URL environment variable, defaults to localhost:3000 if not present
-    @Value("${FRONTEND_URL:http://localhost:3000}")
+    @Value("${FRONTEND_URL:https://skillsfusion.vercel.app/}")
     private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "http://localhost:3000")
+                .allowedOrigins(frontendUrl, "http://localhost:3000","https://skillsfusion.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
