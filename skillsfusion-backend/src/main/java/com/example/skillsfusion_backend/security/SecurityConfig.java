3b48690ba3b99dfc7eb8d6d89233e5ae6f23ae88
Hari Shankar <harishankarhs351@gmail.com>
2026-09-10 13:27:26 +0530

    Permit public access to /api/auth/** and configure CORS in SecurityConfig

--- skillsfusion-backend/src/main/java/com/example/skillsfusion_backend/security/SecurityConfig.java
+++ skillsfusion-backend/src/main/java/com/example/skillsfusion_backend/security/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.example.skillsfusion_backend.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
